2f4b0804749a82e44228f926082f610d634ad4b1
u410061 <u410061@192.168.1.35>
2024-01-19 19:31:09 +0530

    Committing changes

---
+++ Test_2/src/main/java/com/appian/robot/core/template/RobotBlankTemplate.java
@@ -0,0 +1,15 @@
+
+
+
+package com.appian.robot.core.template;
+
+import com.novayre.jidoka.client.api.IRobot;
+import com.novayre.jidoka.client.api.annotations.Robot;
+
+/**
+ * The Class RobotBlankTemplate.
+ */
+@Robot
+public class RobotBlankTemplate implements IRobot {
+
+}
